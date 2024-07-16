bd18d61ef077662669ad678b87645d17a056a330
garden <wunsoho@naver.com>
2024-07-16 16:24:35 +0900

    더블클릭 리스트

---
+++ LDRA_Parser/Model/ViolationItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDRA_Parser.Model
+{
+    public class ViolationItem
+    {
+        public string ViolationNumber { get; set; }
+        public string Location { get; set; }
+    }
+}
