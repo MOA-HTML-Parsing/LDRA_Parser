be4f8cb4a40f324e13bb913457d96797747b8a99
Leejh0426 <leejunho2000@gmail.com>
2024-07-16 14:54:52 +0900

    #26 큰 틀 완성

--- LDRA_Parser/MainWindow.xaml.cs
+++ LDRA_Parser/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace LDRA_Parser
     public partial class MainWindow : Window
     {
         private FileSystemViewModel _viewModel;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -127,6 +126,19 @@ namespace LDRA_Parser
             }
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+          
+            var BeforeItems = BeforeList.ItemsSource as IEnumerable<BeforeItem>;
+            var AfterItems = AfterList.ItemsSource as IEnumerable<AfterItem>;
 
+
+
+
+            _viewModel.compareBeforeAfter(BeforeItems,AfterItems);
+
+        }
+
+    
     }
 }
